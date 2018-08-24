8e02f73deb129fb8424464bbc8b6521b85c05eed
avinashflexi <avinash.udumula@gmail.com>
2018-08-24 10:55:00 +0530

    adding my second code

---
+++ figlet_format.py
@@ -0,0 +1,22 @@
+from pyfiglet import figlet_format
+from termcolor import colored
+
+
+def print_art(msg,color):
+	valid_colors = ("red", "green","yellow","blue","magenta","cyan","white")
+
+
+
+	if color not in valid_colors:
+		color="magenta"
+
+
+	ascii_art = figlet_format(msg)
+	colored_ascii = colored(ascii_art, color=color)
+	print(colored_ascii)	  
+
+
+
+msg=input("what would you like to print?")
+color = input ("what color?")
+print_art(msg, color)
